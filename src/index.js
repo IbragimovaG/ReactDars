221b7f42718b7e0168661404c25e55caf9e4aa72
IbragimovaG <ibragimoffa.gul0706@gmail.com>
2024-11-28 16:46:38 +0500

    counter qoshildi

--- src/index.js
+++ src/index.js
@@ -2,13 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 
 class Index extends React.Component {
+    state = {
+        count:0
+
+    }
+
+   addCount=()=>{
+       this.setState({
+           count:this.state.count + 1
+       })
+}
+removeCount=()=>{
+        this.setState({
+            count:this.state.count - 1
+        })
+    }
+
+
     render() {
         return <div>
-            .card
+            <div className={'card'}>
+                <div className={'card-header'}>
+                    <h3>Counter</h3>
+                </div>
+                <div className={'card-body'}>
+                <h1>{this.state.count}</h1>
+                </div>
+                <div className={'card-footer'}>
+                    <button className={'btn btn-success'} onClick={this.addCount}>+</button>
+                    <button className={'btn btn-danger ml-1'} onClick={this.removeCount}>-</button>
+
+                </div>
+            </div>
         </div>
     }
 }
+
 ReactDOM.render(
-    <Index />,
+    <Index/>,
     document.getElementById('root')
 )
